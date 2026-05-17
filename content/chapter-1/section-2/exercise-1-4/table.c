ab263fa7917003803ae2bdc539e2b12bbed213f9
Benjamin Philip <benjamin.philip495@gmail.com>
2026-05-17 20:43:09 +0530

    Exercise 1-4

---
+++ content/chapter-1/section-2/exercise-1-4/table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* print Celsius-Fahrenheit table
+   for celsius = 0, 20, ..., 300 */
+main()
+{
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0; /* lower limit of temperature table */
+    upper = 300; /* upper limit */
+    step = 20; /* step size */
+
+    printf(" C     F  \n");
+
+    celsius = lower;
+    while (celsius <= upper) {
+        fahr = ((9.0 / 5.0) * celsius) + 32;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
